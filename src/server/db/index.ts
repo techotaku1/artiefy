2483e2c700eb6f5d36b6c8d73778665091a9bb82
CristianAGarces <cristyan@users.noreply.github.com>
2024-12-23 13:41:11 -0500

    actualizacion cambios

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-=======
-//src\server\db\index.ts
->>>>>>> origin/main
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
@@ -14,15 +10,7 @@ import * as schema from "./schema";
 config({ path: ".env" });
 
 // Inicializa la conexión a Neon usando la URL de la base de datos
-<<<<<<< HEAD
 const sql = neon(process.env.DATABASE_URL!);
 
 // Exporta la conexión Drizzle para usarla en otros lugares del proyecto
 export const db = drizzle(sql, { schema });
-=======
-const sql = neon(process.env.POSTGRES_URL!);
-
-// Exporta la conexión Drizzle para usarla en otros lugares del proyecto
-export const db = drizzle(sql, { schema });
-
->>>>>>> origin/main
